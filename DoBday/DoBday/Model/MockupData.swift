2ebe0c3549931607959f21d71abe07718d7379b5
Sohyeon Lee <teabag759@gmail.com>
2024-10-17 17:32:12 +0900

    [SETTING/#4] 목업 데이터 생성

---
+++ DoBday/DoBday/Model/MockupData.swift
@@ -0,0 +1,16 @@
+//
+//  MockupData.swift
+//  DoBday
+//
+//  Created by 이소현 on 10/17/24.
+//
+
+import Foundation
+import SwiftUI
+
+let mockBdayData: [Bday] = [
+    Bday(id: UUID(), name: "소이", profileImage: Image("soy"), dateOfBday: Date(), isLunar: false, notiFrequency: "Weekly"),
+    Bday(id: UUID(), name: "씨네필", profileImage: Image("cinephil"), dateOfBday: Calendar.current.date(byAdding: .day, value: 7, to: Date()), isLunar: false, notiFrequency: "Daily"),
+    Bday(id: UUID(), name: "피카", profileImage: Image("pika"), dateOfBday: Calendar.current.date(byAdding: .month, value: 1, to: Date()), isLunar: true, notiFrequency: "Monthly"),
+    Bday(id: UUID(), name: "레디", profileImage: Image("ready"), dateOfBday: Calendar.current.date(byAdding: .month, value: -3, to: Date()), isLunar: false, notiFrequency: "Weekly")
+]
