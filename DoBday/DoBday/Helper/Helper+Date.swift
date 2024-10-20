feef923dcda5495454b8e71da5ff64fdff46166c
hajizn-pi <springlhj2@gmail.com>
2024-10-21 01:56:49 +0900

    [Feat/#27] 프론트엔드+다중선택 로직

---
+++ DoBday/DoBday/Helper/Helper+Date.swift
@@ -0,0 +1,18 @@
+//
+//  Helper+Date.swift
+//  DoBday
+//
+//  Created by Hajin on 10/20/24.
+//
+
+import SwiftUI
+
+struct Helper_Date: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Helper_Date()
+}
