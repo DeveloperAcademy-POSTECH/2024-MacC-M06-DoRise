2cd07f88fb9d5da7669eb34f0d1e8d4b64df1983
cho2-0923 <cho2-@naver.com>
2024-11-03 18:21:53 +0900

    [FEAT/#48] Helper+Fonts 파일에 사용하는 폰트 입력하기

--- DoBday/DoBday/Helper/Helper+Fonts.swift
+++ DoBday/DoBday/Helper/Helper+Fonts.swift
@@ -18,27 +18,53 @@ enum Pretendard: String {
     case regular = "Pretendard-Regular"
     case semiBold = "Pretendard-SemiBold"
     case thin = "Pretendard-Thin"
+    case dateTitle = "Zen Dots"
 }
 
 extension Font {
-    // TODO: 폰트 추가하기
-    static let BdayLargeTitle = Font.custom("Pretendard-Black", size: 48)
+    // MARK: - Large Title
+    static let ltEmphasized = Font.custom("Pretendard-Bold", size: 48)
+    static let ltRegular = Font.custom("Pretendard-Regular", size: 48)
+    
+    // MARK: - Title1
+    static let t1Emphasized = Font.custom("Pretendard-Bold", size: 36)
+    static let t1Regular = Font.custom("Pretendard-Regular", size: 36)
 
-    static let BdayTitle1 = Font.custom("Pretendard-Bold", size: 36)
+    // MARK: - Title2
+    static let t2Emphasized = Font.custom("Pretendard-Bold", size: 22)
+    static let t2Regular = Font.custom("Pretendard-Regular", size: 22)
 
-    static let BdayButton = Font.custom("Pretendard-ExtraBold", size: 16)
-
-    static let BdayoBody = Font.custom("Pretendard-ExtraLight", size: 12)
-
-    static let BdayBody1 = Font.custom("Pretendard-Light", size: 10)
-
-    static let BdayBody2 = Font.custom("Pretendard-Medium", size: 10)
-
-    static let BdayoBody3 = Font.custom("Pretendard-Regular", size: 10)
-
-    static let BdayBody4 = Font.custom("Pretendard-SemiBold", size: 10)
-
-    static let BdayBody5 = Font.custom("Pretendard-Thin", size: 10)
-
+    // MARK: - Title3
+    static let t3Emphasized = Font.custom("Pretendard-Bold", size: 18)
+    static let t3Regular = Font.custom("Pretendard-Regular", size: 18)
+    
+    // MARK: - Headline
+    static let headEmphasized = Font.custom("Pretendard-Bold", size: 16)
+    // MARK: - Body
+    static let bodyEmphasized = Font.custom("Pretendard-SemiBold", size: 16)
+    static let bodyRegular = Font.custom("Pretendard-Regular", size: 16)
+    
+    // MARK: - Callout
+    static let  callEmphasized = Font.custom("Pretendard-Bold", size: 14)
+    static let  collRegular = Font.custom("Pretendard-Regular", size: 14)
+    
+    // MARK: - Sub Headline
+    static let subEmphasized = Font.custom("Pretendard-Bold", size: 13)
+    static let subRegular = Font.custom("Pretendard-Regular", size: 13)
+    
+    // MARK: - Foot Note
+    static let footEmphasized = Font.custom("Pretendard-Bold", size: 12)
+    static let footRegular = Font.custom("Pretendard-Regular", size: 12)
+    
+    // MARK: - Caption1
+    static let c1Emphasized = Font.custom("Pretendard-Bold", size: 11)
+    static let c1Regular = Font.custom("Pretendard-Regular", size: 11)
+    
+    // MARK: - Caption2
+    static let c2Emphasized = Font.custom("Pretendard-Bold", size: 10)
+    static let c2Regular = Font.custom("Pretendard-Regular", size: 10)
+    
+    // MARK: - DateFormat Title
+    /// UpcomingView에 사용할 때 lineSpacing(25) 적용하고 사용하면 똑같아짐
+    static let bdayEmphasized = Font.custom("Zen dots", size: 32)
 }
-
