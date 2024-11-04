2b70c0ae97a028f304a5c31c212b4662b7ae5917
soy <teabag759@gmail.com>
2024-11-05 08:23:09 +0900

    [FEAT/#59] SaveGiftView textEditor에 커서가 올라갈 때 키보드와 화면이 간섭하지 않도록 처리

--- DoBday/DoBday/View/SaveGiftView.swift
+++ DoBday/DoBday/View/SaveGiftView.swift
@@ -13,70 +13,102 @@ struct SaveGiftView: View {
     
     // MARK: - Property
     @Environment(\.modelContext) var context
-//    @Bindable var bdayGift: BdayGift
-//    @Query var bdayGift: BdayGift
-    
-//    var bdayGift: BdayGift
-    
     @State var giftName: String = ""
     @State var giftPrice: String = ""
     @State var memo: String = ""
     @State private var selectedItem: PhotosPickerItem?
     
+    @Namespace var bottomID
+    
+    @FocusState var isFocused: Bool
+    
+    
+    //    @Bindable var bdayGift: BdayGift
+    //    @Query var bdayGift: BdayGift
+    //    var bdayGift: BdayGift
     
     var body: some View {
-        VStack {
-            PhotosPicker(selection: $selectedItem, matching: .images) {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 12)
-                        .frame(width: 140, height: 160)
-                        .foregroundStyle(.gray.opacity(0.2))
+        ScrollViewReader { proxy in
+            ScrollView {
+                VStack {
+                    PhotosPicker(selection: $selectedItem, matching: .images) {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 12)
+                                .frame(width: 140, height: 160)
+                                .foregroundStyle(.gray.opacity(0.2))
+                            
+                            Image(systemName: "photo.badge.plus")
+                                .font(.largeTitle)
+                                .foregroundStyle(.gray)
+                        }
+                    }
+                    .padding(.bottom, 8)
                     
-                    Image(systemName: "photo.badge.plus")
-                        .font(.largeTitle)
-                        .foregroundStyle(.gray)
+                    HStack {
+                        makeIsToBeGivenButton(text: "받은 선물")
+                        makeIsToBeGivenButton(text: "준 선물")
+                    }
+                    .padding(.bottom)
+                    
+                    
+                    VStack(alignment: .leading) {
+                        makeTextAndField(text: "선물 이름", textField: $giftName)
+                            .focused($isFocused)
+                            .padding(.bottom, 16)
+                        
+                        makeTextAndField(text: "가격", textField: $giftPrice)
+                            .focused($isFocused)
+                            .padding(.bottom, 16)
+                        
+                        
+                        Text("메모")
+                            .font(.bday_bodyEmphasized)
+                        TextEditor(text: $memo)
+                            .focused($isFocused)
+                            .scrollContentBackground(.hidden)
+                            .padding()
+                            .background(.gray.opacity(0.2))
+                            .clipShape(RoundedRectangle(cornerRadius: 12))
+                            .id(bottomID)
+                    }
+                    .onTapGesture {
+                        withAnimation {
+                            proxy.scrollTo(bottomID, anchor: .bottom)
+                        }
+                    }
+                    
                 }
-            }
-            .padding(.bottom, 8)
-            
-            HStack {
-                makeIsToBeGivenButton(text: "받은 선물")
-                makeIsToBeGivenButton(text: "준 선물")
-            }
-            .padding(.bottom)
-            
-            
-            VStack(alignment: .leading) {
-                makeTextAndField(text: "선물 이름", textField: $giftName)
-                    .padding(.bottom, 16)
-                makeTextAndField(text: "가격", textField: $giftPrice)
-                    .padding(.bottom, 16)
+                .padding()
+                .navigationTitle("주고 싶은 선물")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button {
+                            
+                        } label: {
+                            Text("추가")
+                        }
+                        
+                        
+                    }
+                }
                 
-                Text("메모")
-                    .font(.bday_bodyEmphasized)
-                TextEditor(text: $memo)
-                    .scrollContentBackground(.hidden)
-                    .padding()
-                    .background(.gray.opacity(0.2))
-                    .clipShape(RoundedRectangle(cornerRadius: 12))
+            }
+            .onChange(of: isFocused) { oldValue, newValue in
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.22) {
+                    withAnimation {
+                        proxy.scrollTo(bottomID, anchor: .center)
+                    }
+                }
+            }
+            .onTapGesture {
+                hideKeyboard()
             }
             
-        }
-        .padding()
-        .navigationTitle("주고 싶은 선물")
-        .navigationBarTitleDisplayMode(.inline)
-        .toolbar {
-            ToolbarItem(placement: .topBarTrailing) {
-                Button {
-                    
-                } label: {
-                    Text("추가")
-                }
-
-            }
         }
     }
 }
+
 
 
 // MARK: - extension SaveGiftView
@@ -104,17 +136,17 @@ extension SaveGiftView {
     }
 }
 
-//#Preview {
-//    NavigationStack {
-//        SaveGiftView(bdayGift: sampleGifts.first ?? BdayGift(
-//            id: UUID(),
-//            isToBeGiven: true,
-//            giftName: "꽃다발",
-//            giftPrice: "₩30,000",
-//            giftImage: nil,
-//            memo: "생일에 맞춰 전달 예정",
-//            giftURL: "https://flowershop.com/bouquet"
-//        ))
-//    }
-//    .modelContainer(for: BdayGift.self)
-//}
+
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+
+
+#Preview {
+    NavigationStack {
+        SaveGiftView()
+    }
+    .modelContainer(for: BdayGift.self)
+}
