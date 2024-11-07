0cd4ba342327d7c932c4ab6ac47d269ff80d59f6
hajizn-pi <springlhj2@gmail.com>
2024-11-07 18:32:40 +0900

    [FIX/#71] SetDateView 리펙토링 처리

---
+++ DoBday/DoBday/View/SetDateView/DatePickerOfSetDateView.swift
@@ -0,0 +1,23 @@
+//
+//  DatePickerOfSetDateView.swift
+//  DoBday
+//
+//  Created by Hajin on 11/7/24.
+//
+import SwiftUI
+
+struct DatePickerOfSetDateView: View {
+
+    @Binding var dateOfBday: Date
+
+    var body: some View {
+        VStack {
+            DatePicker("Please enter a date", selection: $dateOfBday, displayedComponents: .date)
+                .datePickerStyle(WheelDatePickerStyle())
+                .labelsHidden()
+                .environment(\.locale, Locale.init(identifier: "ko"))
+                .padding(0)
+
+        }
+    }
+}
